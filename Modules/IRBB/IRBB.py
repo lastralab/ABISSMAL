678541f887fd5b5206df62ca05b6f5ffb76f5f02
Grace Smith Vidaurre <gsmithvi@gmail.com>
2021-11-13 16:05:38 -0500

    Updated README
    Moved descriptive text to script header and README

--- Modules/IRBB/IRBB.py
+++ Modules/IRBB/IRBB.py
@@ -1,15 +1,8 @@
 # Created by PyCharm
 # Author: nmoltta
+# Contributor: G. Smith-Vidaurre
 # Project: ParentalCareTracking
 # Date: 11/7/21
-
-# G. Smith-Vidaurre 10 Nov 2021:
-# Modified from https://simonprickett.dev/using-a-break-beam-sensor-with-python-and-raspberry-pi/
-# This initial code works but needs to be modified to get time the beam was broken, and save these per day
-# I'd like 2 beam breakers per nest container, but first need to get the code working for a single sensor
-# Lead sensor is the first sensor from the tunnel entrance (e.g. from outside into nest chamber)
-# Rear sensor will be behind the lead, the closest to the nest chamber entrance)
-#sensor_ID = "lead" 
 
 import RPi.GPIO as GPIO
 
@@ -21,7 +14,7 @@ def break_beam_callback(channel):
         print("beam unbroken")
     else:
         print("beam broken")
-        
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BEAM_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.add_event_detect(BEAM_PIN, GPIO.BOTH, callback=break_beam_callback)
--- Modules/IRBB/ReadMe.md
+++ Modules/IRBB/ReadMe.md
@@ -1,1 +1,8 @@
 <p>Placeholder to describe functions in this directory</p>
+
+Modified from https://simonprickett.dev/using-a-break-beam-sensor-with-python-and-raspberry-pi/
+This initial code works but needs to be modified to get time the beam was broken, and save these per day
+I'd like 2 beam breakers per nest container, but first need to get the code working for a single sensor
+Lead sensor is the first sensor from the tunnel entrance (e.g. from outside into nest chamber)
+Rear sensor will be behind the lead, the closest to the nest chamber entrance)
+sensor_ID = "lead"
