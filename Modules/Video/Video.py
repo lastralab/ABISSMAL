c27ec83df349d4281afeb5b40bf12b1769ef4c64
Grace Smith Vidaurre <gsmithvi@gmail.com>
2021-11-13 17:27:24 -0500

    Data and description
    Details about device, notes on usage, and desired data

--- Modules/Video/Video.py
+++ Modules/Video/Video.py
@@ -1,3 +1,8 @@
+# Created by PyCharm
+# Author: nmoltta, gsvidaurre
+# Project: ParentalCareTracking
+# Date: 11/13/21
+
 import picamera
 import time
 from datetime import datetime
@@ -18,29 +23,28 @@ f = open(os.path.join(out_path, log_file_name), "a")
 
 # Perform video recording for a given duration of time
 camera = picamera.PiCamera()
-                    
+
 print("recording\n", file = f)
 print(int(str(datetime.now().hour) + str(datetime.now().minute).zfill(2)), file = f)
 print("\n", file = f)
-                    
+
 # Reasonable iso values are 100 and 200 for daytime, 400 and 800 for low light
 camera.iso = 400
 # 1280x720 resolution is a 16:9 wide-screen aspect, looks pixelated
-# anything x 1080 throws an error 
+# anything x 1080 throws an error
 # 1024x768 is a 4:3 aspect ratio
 # Will need to play around with this to set width and height with respect to how high cameras are above birds once in cages
-camera.resolution = (1000, 1000) 
+camera.resolution = (1000, 1000)
 camera.framerate = 60
 camera.rotation = 180
 camera.start_recording(file_name)
 # camera.wait_recording(60 * 30) # record for 30 minutes while checking for errors
 camera.wait_recording(60*2) # testing with 60 second video
 camera.stop_recording()
-                    
+
 print("done video recording\n", file = f)
 print(int(str(datetime.now().hour) + str(datetime.now().minute).zfill(2)), file = f)
 print("\n", file = f)
 
 # Close the file connection used to write output from print statements
 f.close()
-
