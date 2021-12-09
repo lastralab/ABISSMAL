a340e3d372ce1ee983e3a99fe452eb972fdc75d7
gsvidaurre <gsvidaurre@gmail.com>
2021-12-08 22:51:35 -0500

    [PCT-20] Trying to make Python script run indefinitely

--- Main.sh
+++ Main.sh
@@ -1,8 +1,46 @@
-# terminal run:
-# .../pathto/ParentalCareTracking/Main.sh
+#!/bin/bash
+# Terminal run:
+# $ sudo apt-get install screen
+# $ cd <<path to git directory>>/ParentalCareTracking/
+# $ chmod +x Main.sh
+
+RED='\033[0;31m'
+NC='\033[0m'
+BIGreen='\033[1;92m'
+Green='\033[0;92m'
+Yellow='\033[0;93m'
+Blue='\033[0;94m'
+Purple='\033[0;95m'
+Cyan='\033[0;96m'
+
+user_name=$(whoami)
+location=$(pwd)
+python_v=$(which python)
+irbb_file="/Modules/IRBB.py"
+irbb_command="${python_v} -c ${location}${irbb_file}"
+
+echo ""
+echo -e "${Blue}Project:${NC}     ${Green}PARENTAL CARE TRACKING ${NC}"
+echo -e "${Blue}Repository:${NC}  https://github.com/lastralab/parentalcaretracking"
+echo -e "${Blue}Date:${NC}        ${Blue}November 2021${NC}"
+echo -e "${Blue}Authors:${NC}     ${Cyan}Tania Molina & Grace Smith Vidaurre${NC}"
+echo ""
+
+echo -e "${Yellow}Setting permissions...${NC}"
+find Modules/ -type f -exec chmod 644 {} \;
+chown -R ${user_name} .
+echo -e "${Yellow}Cleaning up detached screens...${NC}"
+#screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
+echo ""
+echo -e "Starting screen name: ${Cyan}irbb${NC}..."
+sleep 1s
+
+screen -S irbb bash -c "${irbb_command}"
+
+echo -e "Started ${Cyan}irbb${NC}."
+#screen -r irbb
+
+echo ""
 
 # TODO: incorporate all modules to run on separate screens
-# more info: https://raspi.tv/2012/using-screen-with-raspberry-pi-to-avoid-leaving-ssh-sessions-open
-# screen bash -d -m -S irbb python Modules/IRBB.py
 
-# TODO: include top command to track system performance
--- Modules/IRBB.py
+++ Modules/IRBB.py
@@ -3,6 +3,8 @@
 # Project: ParentalCareTracking
 # Date: 11/16/2021
 
+#!/usr/bin/env python3
+
 import time
 import signal
 from datetime import datetime
@@ -13,6 +15,7 @@ import logging
 from helper import logger_setup
 from helper import csv_writer
 from helper import box_id
+from time import sleep
 
 logger_setup('/home/pi/')
 
@@ -26,17 +29,21 @@ header = ['chamber_id', 'sensor_id', 'year', 'month', 'day', 'timestamp']
 sensor_id = "rear"
 irbb_data = "/home/pi/Data_ParentalCareTracking/IRBB"
 
+logging.info('started irbb script')
 
 def detect_beam_breaks_callback(BEAM_PIN, sensor_id):
-     if GPIO.input(BEAM_PIN):
-         logging.info('No IRBB activity on pin ' + str(BEAM_PIN) + '.')
-     else:
-         dt = datetime.now()
-         logging.info('IRBB activity detected at: ' + f"{dt:%H:%M:%S.%f}")
-         # This finally prints a .csv but the callback isn't working
-         csv_writer(str(box_id), 'IRBB', irbb_data, f"{dt.year}_{dt.month}_{dt.day}",
-                    header, [box_id, sensor_id, f"{dt.year}", f"{dt.month}", f"{dt.day}", f"{dt:%H:%M:%S.%f}"])
-         # TODO implement video function
+    while True:
+         if GPIO.input(BEAM_PIN):
+             #logging.info('No IRBB activity on pin ' + str(BEAM_PIN) + '.')
+             continue
+         else:
+             dt = datetime.now()
+             logging.info('IRBB activity detected at: ' + f"{dt:%H:%M:%S.%f}")
+             # This finally prints a .csv but the callback isn't working
+             csv_writer(str(box_id), 'IRBB', irbb_data, f"{dt.year}_{dt.month}_{dt.day}",
+                        header, [box_id, sensor_id, f"{dt.year}", f"{dt.month}", f"{dt.day}", f"{dt:%H:%M:%S.%f}"])
+             # TODO implement video function
+             sleep(1)
 
 
  # Handler function for manual Ctrl + C cancellation
@@ -48,6 +55,7 @@ def signal_handler(sig, frame):
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BEAM_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-
 GPIO.add_event_detect(BEAM_PIN, GPIO.FALLING, callback=lambda x: detect_beam_breaks_callback(BEAM_PIN, sensor_id),
                                       bouncetime=100)
+
+detect_beam_breaks_callback(BEAM_PIN, sensor_id)
