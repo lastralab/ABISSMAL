eecad5335b09c2eb1bc055b8cb80c60920b23567
lastralab <lastralab@gmail.com>
2021-12-21 15:26:59 -0500

    [PCT-19] refactor main.sh and updated ReadMe

--- Main.sh
+++ Main.sh
@@ -8,7 +8,6 @@
 
 RED='\033[0;31m'
 NC='\033[0m'
-BIGreen='\033[1;92m'
 Green='\033[0;92m'
 Yellow='\033[0;93m'
 Blue='\033[0;94m'
@@ -35,34 +34,32 @@ echo ""
 echo -e "${Blue}Project:${NC}     ${Green}PARENTAL CARE TRACKING ${NC}"
 echo -e "${Blue}Repository:${NC}  https://github.com/lastralab/parentalcaretracking"
 echo -e "${Blue}Date:${NC}        ${Blue}November 2021${NC}"
-echo -e "${Blue}Authors:${NC}     ${Cyan}Tania Molina & Grace Smith Vidaurre${NC}"
+echo -e "${Blue}Authors:${NC}     ${Cyan}Tania M. Molina & Grace Smith-Vidaurre${NC}"
 echo ""
 
-echo -e "${Yellow}Setting permissions...${NC}"
+echo -e "${Yellow}Setting permissions for user:${NC} ${user_name}${NC}"
+echo ""
 find Modules/ -type f -exec chmod 644 {} \;
-chown -R ${user_name} .
-echo -e "${Yellow}Cleaning up detached screens...${NC}"
+chown -R "${user_name}" .
+echo -e "${Green}To access a screen run:${NC} screen -r ${Purple}{name}${NC}"
+echo -e "${RED}To detach a screen press Ctrl + D${NC}"
 #screen -ls | grep Detached | cut -d. -f1 | awk '{print $1}' | xargs kill
 echo ""
 
 echo -e "Starting screen name: ${Cyan}irbb${NC}..."
 sleep 1s
 screen -dmS irbb bash -c "${irbb_command}"
-echo -e "Started ${Purple}irbb${NC}."
 
 echo -e "Starting screen name: ${Cyan}video${NC}..."
 sleep 1s
 screen -dmS video bash -c "${video_command}"
-echo -e "Started ${Purple}video${NC}."
 
 echo -e "Starting screen name: ${Cyan}rfid${NC}..."
 sleep 1s
 screen -dmS rfid bash -c "${rfid_command}"
-echo -e "Started ${Purple}rfid${NC}."
 
 echo -e "Starting screen name: ${Cyan}temp${NC}..."
 sleep 1s
 screen -dmS temp bash -c "${temp_command}"
-echo -e "Started ${Purple}temp${NC}."
 
 echo ""
