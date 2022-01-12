8883f19271af227669de7a15c532bc6ee45fbd79
gsvidaurre <gsvidaurre@gmail.com>
2022-01-11 19:28:06 -0500

    [PCT-18] Added Video .csv; updating code to combine and convert videos; still getting ffmpeg error

--- Modules/Video.py
+++ Modules/Video.py
@@ -8,10 +8,13 @@
 # Should we be saving an entry to a .csv file every time a video is recorded??
 # To avoid RPi freezing always cancel with Ctrl + C, not "Stop"
 
+import os
 import time
 import signal
 from datetime import datetime
 import sys
+import csv
+import re
 import RPi.GPIO as GPIO
 import logging
 import io
@@ -22,7 +25,6 @@ from helper import csv_writer
 from helper import box_id
 from time import sleep
 from os import walk
-#from picamera import PiCamera
 from PIL import Image
 from subprocess import call
 from pathlib import Path
@@ -34,10 +36,11 @@ logger_setup('/home/pi/')
 # Check out "lightweight Python motion detection" https://forums.raspberrypi.com/viewtopic.php?t=45235&sid=b32ca9b34abf8243de9b15ddff22faf0
 
 path = "/home/pi/Data_ParentalCareTracking/Video/"
+header = ['chamber_id', 'year', 'month', 'day', 'time_video_started', 'video_file_name']
 prior_image = None
 
 # Time range (military) during which video will be recorded
-time_range = np.array([6, 19])
+time_range = np.array([6, 20])
 
 # Video settings
 video_width = 640
@@ -77,10 +80,8 @@ def detect_motion(camera):
         current_image = Image.open(stream)
         buffer2 = current_image.load()
         
-        # Compare current_image to prior_image to detect motion
-        # Count changed pixels
+        # Count changed pixels to compare current_image to prior_image to detect motion
         changedPixels = 0
-        # xrange no longer exists in Python 3
         
         # Loop over the pixels expected in the x and y dimensions
         # Detect changes in pixels in all 3 color channels
@@ -105,13 +106,42 @@ def detect_motion(camera):
         return result
     
     
-def convert_video(filename):
-    #file_mp4 = path + str(box_id) + dt_str + '.mp4'
-    file_mp4 = Path(filename).stem + '.mp4'
-    command = "MP4Box -add " + filename + " " + file_mp4
-    call([command], shell=True)
-    logging.info('Converted video ' + filename + ' to mp4.')
+# TKTK getting ffmpeg error "101_2022_1_11_18_42_13_pre-trigger.mp4: Invalid data found when processing input"
+# I also get a warning "WARNING: library configuration mismatch"
+# ffmpeg version 4.1.6-1
+# Might need to update ffmpeg
 
+# 10 Jan 2021
+# sudo apt purge --autoremove -y ffmpeg
+# sudo apt update
+# sudo apt install -y ffmpeg
+# ffmpeg -version
+# ffmpeg version 4.1.8-0+deb10u1+rpt1 Copyright (c) 2000-2021
+
+# Combining .h264: [concat @ 0x1a3f280] Line 4: unknown keyword '?'d'
+#/home/pi/Data_ParentalCareTracking/Video/101_2022_1_11_19_15_14_pre_trigger.h264: Invalid data found when processing input
+
+def convert_video(filename):
+    file_mp4 = path + Path(filename).stem + '.mp4'
+    command = "MP4Box -add " + filename + " " + file_mp4
+    call([command], shell = True)
+    logging.info('Converted video ' + filename + ' to mp4.')
+    #os.remove(filename)
+
+def combine_video(file1, file2):
+    #file1 = path + Path(file1).stem + ".mp4"
+    #file2 = path + Path(file2).stem + ".mp4"
+    out_file = re.sub(r"_post_trigger", "", file2)
+    #out_file = Path(out_file).stem + ".mp4"
+    in_files = file1 + " " + file2
+    os.system(r"/bin/ffmpeg" + " -f concat -i " + in_files + " -safe 0 -c copy " + out_file)
+    csv_writer(str(box_id), 'Video', path, f"{dt.year}_{dt.month}_{dt.day}",
+               header, [box_id, f"{dt.year}", f"{dt.month}", f"{dt.day}", f"{dt:%H:%M:%S.%f}", out_file])
+    #os.remove(file1)
+    #os.remove(file2)
+    
+    convert_video(out_file)
+             
 
 with picamera.PiCamera() as camera:
     general_time = datetime.now()
@@ -136,8 +166,8 @@ with picamera.PiCamera() as camera:
                     # record the frames "after" motion
                     dt = datetime.now()
                     dt_str = str(f"{dt.year}_{dt.month}_{dt.day}_{dt:%H}_{dt:%M}_{dt:%S}")
-                    file1_h264 = path + str(box_id) + "_" + dt_str + "_pre-trigger" + '.h264'
-                    file2_h264 = path + str(box_id) + "_" + dt_str + "_post-trigger" + '.h264'
+                    file1_h264 = path + str(box_id) + "_" + dt_str + "_pre_trigger" + '.h264'
+                    file2_h264 = path + str(box_id) + "_" + dt_str + "_post_trigger" + '.h264'
                     camera.split_recording(file2_h264)
                     # Record for the specified duration of time after motion detection
                     camera.wait_recording(record_duration)
@@ -152,26 +182,15 @@ with picamera.PiCamera() as camera:
                     camera.wait_recording(1)
                     camera.split_recording(stream)
                     
-                    # Downside to conversion here is that will miss movements that occur while files converting
-                    convert_video(file1_h264)
-                    convert_video(file2_h264)
+                    # Downside to conversion and combining here is that will miss movements that occur while files converting
+                    #convert_video(file1_h264)
+                    #convert_video(file2_h264)
+                    
+                    combine_video(file1_h264, file2_h264)
+                                        
                     
         finally:
             camera.stop_recording()
             camera.close()
             GPIO.output(REC_LED, GPIO.LOW)
             
-
-# Convert videos to mp4 as soon as recording hours are over
-# Instead, Tania will write code to convert videos before backups too
-#try:
-#    while True:
-        
-##        time_only = datetime.now().time()
-#        hour_only = int(f"{time_only:%H}")
-        
-#        if(hour_only > 19):
-#            convert_video(file1_h264)
-#            convert_video(file2_h264)
-#        else:
-#            pass
