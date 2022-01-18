1e02730ee9db44014eeac93585003a965909250b
gsvidaurre <gsvidaurre@gmail.com>
2022-01-18 17:54:30 -0500

    [PCT-33] updated back-up logic

--- Modules/Backups.py
+++ Modules/Backups.py
@@ -11,6 +11,8 @@ from helper import logger_setup
 from helper import csv_writer
 from helper import box_id
 from helper import modules
+from helper import video_extension
+from helper import file_extension
 import os
 from datetime import *
 import shutil
@@ -35,40 +37,61 @@ def usb_connected(box_id):
                 # TODO send email
 
 
-def backup_init(dt, date, destination, source):
+def video_backup_init(dt, date, destination, source):
+    src = source + '/Video'
+    path = media_path + destination + '/Data/Video/' + date
+    files = os.listdir(src)
+    if len(files) > 0:
+        if not os.path.exists(path):
+            os.makedirs(path)
+        for filename in files:
+            if filename.endswith(video_extension):
+                shutil.move(os.path.join(source + '/Video/', filename), os.path.join(path, filename))
+                logging.info('Backed-up videos at ' + str(dt.hour) + ':' + str(dt.minute).zfill(2) + 'hrs')
+
+            else:
+                pass
+    else:
+        pass
+                # TKTK prints eternally in log and also prints all module names...
+        #else:
+            #pass
+            
+            
+def csv_backup_init(dt, date, destination, source):
     for module in modules:
-        if(module == "Video"):
-            src = source + '/' + module
-            path = media_path + destination + '/Data/' + module + '/' + date
-            files = os.listdir(src)
-            if len(files) > 0:
-                if not os.path.exists(path):
-                    os.makedirs(path)
-                for filename in files:
-                    shutil.move(os.path.join(source + '/' + module, filename), os.path.join(path, filename))
-                logging.info('Backed-up ' + module + ' data at ' + str(dt.hour) + ':' + str(dt.minute) + 'hrs')
-            else:
-                pass
-        else:
-            src = source + '/' + module
+        src = source + '/' + module
+        files = os.listdir(src)
+        yesterday = dt - timedelta(days = 1)
+        yesterday_file = module + "_" + box_id + "_" + f"{yesterday.year}_{yesterday.month}_{yesterday.day}" + ".csv"
+        if(module != 'Video'):
             path = media_path + destination + '/Data/' + module + '/'
-            files = os.listdir(src)
-            if len(files) > 0:
-                if not os.path.exists(path):
-                    os.makedirs(path)
-                for filename in files:
-                    shutil.move(os.path.join(source + '/' + module, filename), os.path.join(path, filename))
-                logging.info('Backed-up ' + module + ' data at ' + str(dt.hour) + ':' + str(dt.minute).zfill(2) + 'hrs')
-            else:
-                pass
+        else:
+            path = media_path + destination + '/Data/Video/' + yesterday.strftime("%Y_%m_%d")
+        if len(files) > 0:
+            if not os.path.exists(path):
+                os.makedirs(path)
+            for filename in files:
+                if filename.endswith(file_extension):
+                    if(filename == yesterday_file):
+                        shutil.move(os.path.join(source + '/' + module, filename), os.path.join(path, filename))
+                        logging.info('Backed-up ' + module + ' metadata at ' + str(dt.hour) + ':' + str(dt.minute).zfill(2) + 'hrs')
+                    else:
+                        pass
+                else:
+                    pass         
+        else:
+            pass
+
 
 
 try:
     while True:
         now = datetime.now()
-        folder = now.strftime("%m_%d_%Y")
-        if usb_connected(box_id) and now.hour == 15 and now.minute == 19:
-            backup_init(now, folder, box_id, pi_home + data_path)
+        folder = now.strftime("%Y_%m_%d")
+        if usb_connected(box_id) and now.hour == 19 and now.minute == 45:
+            video_backup_init(now, folder, box_id, pi_home + data_path)
+            csv_backup_init(now, folder, box_id, pi_home + data_path)
 except KeyboardInterrupt:
     logging.info('Exiting Backups.py')
 
