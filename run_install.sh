5f105540551ec8ed5b58c1e1211b9f0e1989c324
lastralab <lastralab@gmail.com>
2022-01-25 13:23:25 -0500

    [PCT-76] created install

---
+++ run_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+##
+ # Created by PyCharm
+ # Author: nmoltta
+ # Project: ParentalCareTracking
+ # Date: 01/25/2022
+##
+
+RED='\033[0;31m'
+NC='\033[0m'
+BIGreen='\033[1;92m'
+Green='\033[0;92m'
+Yellow='\033[0;93m'
+Blue='\033[0;94m'
+Purple='\033[0;95m'
+Cyan='\033[0;96m'
+
+user_name=$(whoami)
+location=$(pwd)
+
+
