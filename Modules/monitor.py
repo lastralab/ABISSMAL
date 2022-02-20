95868c78dcdf72d0d6eab11697be0f49a2b53e1a
lastralab <lastralab@gmail.com>
2022-02-20 16:28:28 -0500

    [PCT-104] created monitor

--- Modules/monitor.py
+++ Modules/monitor.py
@@ -5,11 +5,12 @@
 
 import subprocess
 import logging
-from Modules.helper import email_alert
-from Modules.helper import logger_setup
+import time
+from helper import email_alert
+from helper import logger_setup
 
 logger_setup('/home/pi/')
-modules = ['temp', 'video', 'rfid', 'irbb', 'backups']
+modules = ['temp', 'video', 'rfid', 'irbb', 'backup']
 
 logging.info('Starting Monitor script')
 print('Starting Monitor script')
@@ -17,17 +18,23 @@ print('Starting Monitor script')
 
 def monitor_screens():
     screens = str(subprocess.getoutput("screen -list"))
-    for screen in screens:
+    for screen in modules:
         if screens.find(screen) == -1:
             email_alert(screen, 'Screen not running.')
             logging.error('Screen not running: ' + screen)
             print('Screen closed: ' + screen)
+        else:
+            pass
 
 
 try:
     while True:
         monitor_screens()
+        logging.info("Monitored screens.")
+        time.sleep(60)
+        pass
 except Exception as E:
     logging.error('Monitor error: ' + str(E))
     email_alert('Monitor', 'Error while monitoring screens: ' + str(E))
     print('Monitor error: ' + str(E))
+    pass
