a7b1c5037f35f2a26b6a038370f515f1f339d505
lastralab <lastralab@gmail.com>
2022-02-26 17:29:39 -0500

    [PCT-127] log not logging

--- Modules/log.py
+++ Modules/log.py
@@ -7,16 +7,28 @@
 
 import logging
 from importlib import reload
+import os
 
 
 def setup_custom_logger(name):
-    logging.shutdown()
-    reload(logging)
-    formatter = logging.Formatter(fmt="%(asctime)s %(levelname)s %(message)s")
-    handler = logging.StreamHandler()
-    handler.setFormatter(formatter)
-    logging.FileHandler('/home/pi/log/' + name, mode='a', encoding=None, delay=False)
-    logger = logging.getLogger('/home/pi/log/' + name)
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(handler)
+    if os.path.exists('/home/pi/log/' + name):
+        logger = logging.getLogger('/home/pi/log/' + name)
+        format_log = "%(asctime)s %(levelname)s %(message)s"
+        logging.basicConfig(
+            format=format_log,
+            filename='/home/pi/log/' + name,
+            level=logging.DEBUG,
+            datefmt="%Y-%m-%d %H:%M:%S"
+        )
+    else:
+        logging.shutdown()
+        reload(logging)
+        logger = logging.getLogger('/home/pi/log/' + name)
+        format_log = "%(asctime)s %(levelname)s %(message)s"
+        logging.basicConfig(
+            format=format_log,
+            filename='/home/pi/log/' + name,
+            level=logging.DEBUG,
+            datefmt="%Y-%m-%d %H:%M:%S"
+        )
     return logger
