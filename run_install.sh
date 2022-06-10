4d7ed7bf167826e02a324ae7b5619e5331474bb7
lastralab <lastralab@gmail.com>
2022-06-10 12:49:52 -0400

    [staging] run_install fish

--- run_install.sh
+++ run_install.sh
@@ -87,6 +87,7 @@ if [ -n "$packs" ]
 then
 	echo -e "${Yellow}Installing packages:${NC}"
   apt-get update
+  apt-get install fish
   apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
   apt-get install python3
   su -c 'curl -O https://bootstrap.pypa.io/get-pip.py' pi
