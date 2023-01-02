4b124bc621968ebbd5b64fd5247260c72b37a7ca
Tan M3 <64567366+molinainat@users.noreply.github.com>
2023-01-02 16:30:42 -0500

    Update monitor.py
    fix array
    
    Signed-off-by: Tan M3 <64567366+molinainat@users.noreply.github.com>

--- Modules/monitor.py
+++ Modules/monitor.py
@@ -13,16 +13,21 @@ import datetime
 
 dir_setup('/home/pi/')
 modules = []
+flags = []
 
 for module in mods:
     if module == 'Video':
         modules.append('video')
+        flags['video'] = False
     if module == 'IRBB':
         modules.append('irbb')
+        flags['irbb'] = False
     if module == 'RFID':
         modules.append('rfid')
+        flags['rfid'] = False
     if module == 'Temp':
         modules.append('temp')
+        flags['temp'] = False
 
 modules.append('backup')
 
@@ -32,15 +37,17 @@ print('Started Monitor script')
 
 
 def monitor_screens():
+    global flags
     screens = str(subprocess.getoutput("screen -list"))
     for screen in modules:
-        if screens.find(screen) == -1:
-            sms_alert(screen, 'Screen not running.')
+        if screens.find(screen) == -1 and flags[screen] = False:
+            flags[screen] = True;
+            sms_alert('Monitor', 'Screen "' + screen + '" not running.')
             logging.error('Screen not running: ' + screen)
             print('Screen closed: ' + screen)
         else:
             pass
-
+        
 
 try:
     while True:
