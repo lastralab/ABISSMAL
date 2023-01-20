989f34df2156f7a81211ccd9dfe7d981b29b0dfe
lastralab <lastralab@gmail.com>
2023-01-20 13:51:43 -0500

    [main] fixed confusion

--- Main.sh
+++ Main.sh
@@ -71,7 +71,7 @@ filename="/home/pi/log/abissmal_cron.log"
 [[ -f ${filename} ]] || touch ${filename}
 echo -e "${Yellow}/home/pi/log/abissmal_cron.log ready to log cron jobs as${NC} ${user_name}"
 echo -e "To access a screen run:${Green} screen -r ${NC}${Purple}{name}${NC}"
-echo -e "To detach a screen press${Blue} Ctrl + A${NC} then type ${Blue}:${NC} to enter command mode and use command ${RED}\"detach\"${NC}"
+echo -e "To detach a screen press${Blue} Ctrl + A${NC} then type ${RED}:${NC} to enter command mode and use command ${RED}detach${NC}"
 echo ""
 
 echo -e "${Cyan}Enter first letter of the modules to track, separated by a comma:${NC}"
