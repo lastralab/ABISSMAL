756b98dc7a57b6b9c3766b93470cb653d0b8885a
Grace Smith Vidaurre <gsmithvi@gmail.com>
2023-04-27 14:24:58 -0400

    Updated beam breaker logic in integrate_beamBreakers and integrate_beamBreakers_video

--- R/integrate_beamBreakers.R
+++ R/integrate_beamBreakers.R
@@ -1,9 +1,12 @@
-#' @title label_beamBreaker_events
+#' @title integrate_beamBreakers
 #' @description Use data from two pairs of beam breakers to label either entrance and exit movements (in or out of a nest container or nesting area).
 #' 
+#' @param irbb_file_nm A character string. This should be the name of the file that contains all of the pre-processed and labeled beam breaker detections. Each row is a unique detection event. This spreadsheet must contain all the columns specified for the beam breaker data in the subsequent arguments
 #' @param threshold A single numeric value representing a temporal threshold in seconds that will be used to assign events across the beam breaker pairs to unique entrance or exit movements
 #' @param sensor_id_col A character value. This is the name of the metadata column that contains information about the data type (e.g. "sensor_id")
 #' @param timestamps_col A character value. The name of the column that contains timestamps in a format that supports calculations in milliseconds (e.g. "event_datetime_ms")
+#' @param outer_irbb_nm A character value. The label used for the outer pair of beam breakers (e.g. the first pair of beam breakers that an animal encounters when moving into a nest container or area). This should be a value in the column named `sensor_id_col`
+#' @param inner_irbb_nm A character value. The label used for the inner pair of beam breakers (e.g. the second pair of beam breakers that an individual encounters when moving into a nest container or area). This should be a value in the column named `sensor_id_col`
 #' @param path A character string. This should be the path specifying the overall directory where data is saved for a given experimental setup. For instance, "/media/gsvidaurre/Anodorhynchus/Data_Testing/Box_02_31Dec2022/Data".
 #' @param data_dir A character string. This should be the name of directory where the raw data is saved across sensors inside the path above. For instance, "raw_combined".
 #' @param out_dir A character string. This should be the name of a directory specifying where the .csv file of pre-processed data should be saved for each sensor. For instance, "pre-processed". This folder will be appended to the data_path and created as a new directory if it doesn't already exist.
@@ -13,8 +16,8 @@
 #' @return A .csv file with the metadata columns from the original pre-processed data used as input, as well as columns indicating each of the timestamps of the lead and rear beam breaker pairs, a unique label for the given event (e.g. entrance or exit), a unique numeric identifier for the given event, and information about the given data processing stage. Each row in the .csv file is a labeled event across the outer and inner beam breaker pairs that was identified using a given temporal threshold
 #' 
 
-# Make a function to label entrances and exits
-label_beamBreaker_events <- function(threshold, sensor_id_col, timestamps_col, path, data_dir, out_dir, tz, POSIXct_format = "%Y-%m-%d %H:%M:%OS"){
+# Make a function to integrate between beam breaker pairs to infer entrance and exit movements
+integrate_beamBreakers <- function(irbb_file_nm, threshold, sensor_id_col, timestamps_col, outer_irbb_nm, inner_irbb_nm, path, data_dir, out_dir, tz, POSIXct_format = "%Y-%m-%d %H:%M:%OS"){
   
   # Get the current global options
   orig_opts <- options()
@@ -33,7 +36,7 @@ label_beamBreaker_events <- function(threshold, sensor_id_col, timestamps_col, p
   }
   
   # Read in the pre-processed beam breaker data
-  preproc_data <- read.csv(file.path(path, data_dir, "pre_processed_data_IRBB.csv")) %>% 
+  preproc_data <- read.csv(file.path(path, data_dir, irbb_file_nm)) %>% 
     # Make sure that the timestamps are in the right format
     dplyr::mutate(
       timestamp_ms = as.POSIXct(format(as.POSIXct(timestamp_ms, tz = "America/New York"), "%Y-%m-%d %H:%M:%OS6"))
@@ -56,14 +59,18 @@ label_beamBreaker_events <- function(threshold, sensor_id_col, timestamps_col, p
       values_from = !!sym(timestamps_col)
     ) 
   
-  diffs_df <- preproc_data2 %>% 
-    dplyr::select(`Outer Beam Breaker`, `Inner Beam Breaker`) %>% 
+  diffs_df <- preproc_data2 %>%
+    dplyr::select(all_of(outer_irbb_nm), all_of(inner_irbb_nm)) %>% 
     # The lead() call moves the timestamps for the given column one row index up, so that the difference calculation can be performed with the timestamps for the other column
     dplyr::mutate(
+      leading_outer = lead(!!sym(outer_irbb_nm), default = first(!!sym(outer_irbb_nm))),
+      leading_inner = lead(!!sym(inner_irbb_nm), default = first(!!sym(inner_irbb_nm)))                     
+    ) %>% 
+    dplyr::mutate(
       # Here negative differences mean the outer pair triggered first
-      outer_inner_diffs = `Outer Beam Breaker` - lead(`Inner Beam Breaker`, default = first(`Inner Beam Breaker`)),
+      outer_inner_diffs = !!sym(outer_irbb_nm) - leading_inner,
       # Here negative differences mean the inner pair triggered first
-      inner_outer_diffs = `Inner Beam Breaker` - lead(`Outer Beam Breaker`, default = first(`Outer Beam Breaker`))
+      inner_outer_diffs = !!sym(inner_irbb_nm) - leading_outer
     ) %>% 
     # Convert these differences to boolean based on the given threshold (in seconds)
     dplyr::mutate(
@@ -71,57 +78,82 @@ label_beamBreaker_events <- function(threshold, sensor_id_col, timestamps_col, p
       binary_inner_outer_diffs = (abs((round(inner_outer_diffs, 2))) <= threshold)
     ) 
   
-  # Then make a data frame with the timestamps of the entrances and exits identified using the given temporal threshold
-  
-  # Find indices of entrances for outer beam breaker timestamps, or indices of exits for the inner beam breaker timestamps
-  e1_ent <- which(diffs_df$binary_outer_inner_diffs)
-  e1_exi <- which(diffs_df$binary_inner_outer_diffs)
-  
-  # Find indices of entrances for inner beam breaker timestamps, or indices of exits for the outer beam breaker timestamps by adding 1 to the indices above
-  e2_ent <- e1_ent + 1
-  e2_exi <- e1_exi + 1
-  
-  # Use these indices to filter the original pre-processed data frame so that the outer and inner beam breaker timestamps for entrances or exits are placed in the same row
-  ee_df <- data.frame(
-    outer_beamBreaker_timestamp = preproc_data2 %>% 
-      slice(e1_ent) %>% 
-      pull(`Outer Beam Breaker`),
-    inner_beamBreaker_timestamp = preproc_data2 %>% 
-      slice(e2_ent) %>% 
-      pull(`Inner Beam Breaker`),
-    type = "entrance"
-  ) %>% 
-    bind_rows(
-      exi_df <- data.frame(
-        outer_beamBreaker_timestamp = preproc_data2 %>% 
-          slice(e2_exi) %>% 
-          pull(`Outer Beam Breaker`),
-        inner_beamBreaker_timestamp = preproc_data2 %>% 
-          slice(e1_exi) %>% 
-          pull(`Inner Beam Breaker`),
-        type = "exit"
-      )
-    ) %>%
-    dplyr::arrange(outer_beamBreaker_timestamp, desc = FALSE) %>% 
-    rowid_to_column() %>% 
-    dplyr::rename(
-      `unique_beamBreaker_event` = rowid
+  # Make data frames with the timestamps of the entrances and exits identified using the given temporal threshold
+  entrances_df <- diffs_df %>%
+    dplyr::mutate(
+      !!inner_irbb_nm := leading_inner,
+      diffs = outer_inner_diffs
+    ) %>% 
+    # Add back metadata about the beam breaker event labels
+    dplyr::select(all_of(outer_irbb_nm), all_of(inner_irbb_nm), diffs, binary_outer_inner_diffs) %>% 
+    # Then filter for beam breaker events that match the logic for entrances
+    dplyr::filter(
+      binary_outer_inner_diffs
     ) %>% 
     dplyr::mutate(
-      data_stage = "labeling_events",
+      direction_inferred = "entrance"
+    ) %>% 
+    dplyr::select(all_of(outer_irbb_nm), all_of(inner_irbb_nm), diffs, direction_inferred)
+  
+  # Then find exit events
+  exits_df <- diffs_df %>%
+    # Make sure to remove any beam breaker events already identified as entrances from the pool of possible candidates
+    dplyr::filter(
+      !(leading_outer %in% entrances_df[[outer_irbb_nm]]) &
+        !(!!sym(inner_irbb_nm) %in% entrances_df[[inner_irbb_nm]])
+    ) %>% 
+    dplyr::mutate(
+      !!outer_irbb_nm := leading_outer,
+      diffs = inner_outer_diffs
+    ) %>% 
+    dplyr::select(all_of(outer_irbb_nm), all_of(inner_irbb_nm), diffs, binary_inner_outer_diffs) %>% 
+    # Then filter for beam breaker events that match the logic for exits
+    dplyr::filter(
+      binary_inner_outer_diffs
+    ) %>% 
+    dplyr::mutate(
+      direction_inferred = "exit"
+    ) %>% 
+    dplyr::select(all_of(outer_irbb_nm), all_of(inner_irbb_nm), diffs, direction_inferred)
+
+  # Join the data frames and check for duplicated timestamps once more across the entrance and exit events
+  ee_df <- entrances_df %>%
+    bind_rows(
+      exits_df
+    ) %>% 
+    dplyr::arrange(!!sym(outer_irbb_nm), desc = FALSE) %>% 
+    rowid_to_column() %>% 
+    dplyr::rename(
+      `unique_entranceExit` = rowid
+    ) %>% 
+    dplyr::mutate(
+      data_stage = "integration",
       temporal_threshold_s = threshold,
       date_labeled = paste(Sys.Date(), Sys.time(), sep = " ")
     ) %>% 
-    # Add back the original metadata
+    # Rename the outer beam breaker column for the metadata join below
+    dplyr::rename(
+      `timestamp_ms` = !!sym(outer_irbb_nm)
+    ) %>% 
+    # Add back the original metadata (shared between the beam breaker pairs)
     dplyr::inner_join(
       preproc_data %>%
         dplyr::select(-c("sensor_id")),
-      by = c("outer_beamBreaker_timestamp" = "timestamp_ms")
+      by = "timestamp_ms"
     ) %>%
-    dplyr::select(data_type, chamber_id, year, month, day, outer_beamBreaker_timestamp, inner_beamBreaker_timestamp, type, unique_beamBreaker_event, data_stage, temporal_threshold_s, date_labeled)
+    # Redo the renaming the outer beam breaker column
+    dplyr::rename(
+      !!outer_irbb_nm := "timestamp_ms"
+    ) %>% 
+    # Rename the beam breaker columns once more to remove spaces
+    dplyr::rename(
+      `Outer_beam_breaker` = !!sym(outer_irbb_nm),
+      `Inner_beam_breaker` = !!sym(inner_irbb_nm)
+    ) %>% 
+    dplyr::select(data_type, chamber_id, year, month, day, Outer_beam_breaker, Inner_beam_breaker, direction_inferred, unique_entranceExit, data_stage, temporal_threshold_s, date_labeled) %>% 
+    dplyr::arrange(all_of(outer_irbb_nm), desc = FALSE)
   
-  # Save the labeled data
-  write.csv(ee_df, file.path(path, out_dir, "labeled_beamBreaker_data.csv"), row.names = FALSE)
+  write.csv(ee_df, file.path(path, out_dir, "integrated_beamBreaker_data.csv"), row.names = FALSE)
   
   # Reset the current global options
   options(orig_opts)
