aeb0e2f069dda0755ae98304be8188296255f066
Grace Smith Vidaurre <gsmithvi@gmail.com>
2023-04-27 14:24:58 -0400

    Updated beam breaker logic in integrate_beamBreakers and integrate_beamBreakers_video

---
+++ R/README.md
@@ -0,0 +1,9 @@
+
+
+Need to add information about the hardware setup that these functions were written for
+
+One beam breaker pair sits in front of an RFID antenna (outside of the container, the "outer" pair), and the other sits behind the RFID antenna (mounted for detections inside of the container, the "inner" pair). The camera records into the center of the nest container from above. 
+
+
+Then a description of each function
+Possibly add a function to see how much of the original detections were dropped / retained after each pre-processing and integration step?
--- R/tests/testthat/test_label_beamBreaker_events.R
+++ R/tests/testthat/test_label_beamBreaker_events.R
@@ -1,4 +1,20 @@
-ee_df <- read.csv(file.path(path, out_dir, "labeled_beamBreaker_data.csv")) %>% 
+
+library(tidyverse)
+
+irbb_file_nm <- "pre_processed_data_IRBB.csv"
+threshold <- 2
+sensor_id_col <- "sensor_id"
+timestamps_col <- "timestamp_ms"
+outer_irbb_nm <- "Outer Beam Breaker"
+inner_irbb_nm <- "Inner Beam Breaker"
+path <- "/media/gsvidaurre/Anodorhynchus/Data_Testing/Box_02_31Dec2022/Data"
+data_dir <- "pre_processed"
+out_dir <- "integrated"
+tz <- "America/New York"
+POSIXct_format <- "%Y-%m-%d %H:%M:%OS"
+
+
+ee_df <- read.csv(file.path(path, out_dir, "integrated_beamBreaker_data.csv")) %>% 
   # Make sure that the timestamps are in the right format
   dplyr::mutate(
     outer_beamBreaker_timestamp = as.POSIXct(format(as.POSIXct(outer_beamBreaker_timestamp, tz = "America/New York"), "%Y-%m-%d %H:%M:%OS6")),
@@ -23,3 +39,5 @@ ee_df %>%
     min_dff = min(diff),
     max_diff = max(diff)
   )
+
+# Check the distribution of temporal differences between adjacent events as well
