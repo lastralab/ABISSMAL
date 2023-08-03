6b921c5f8f913abbb5f860bdc970747602c9ac4a
Grace Smith Vidaurre <gsmithvi@gmail.com>
2023-08-02 21:05:37 -0400

    Started unit testing for another function; continue with fixing a bug in individual ID columns failing to appear in the output

--- R/tests/testthat/test_score_detectionClusters.R
+++ R/tests/testthat/test_score_detectionClusters.R
@@ -28,10 +28,17 @@ tz <- "America/New York"
 POSIXct_format <- "%Y-%m-%d %H:%M:%OS" 
 
 
-# Here I want to test that the output is the same regardless of which sensor triggered first 
-# Then this test needs to be added to all functions that calculate lags between two different sensors (all of the integration functions). See testing label beam breakers for example code for building these tests
+if (!require(testthat)) install.packages('testthat')
+library(testthat)
 
-test_that("The output is the same regardless of which sensor triggered first", {
+score_detectionClusters <- source("/home/gsvidaurre/Desktop/GitHub_repos/Abissmal/R/score_detectionClusters.R")$value
+
+source("/home/gsvidaurre/Desktop/GitHub_repos/Abissmal/R/utilities.R")
+
+# This testing file can be run by calling test_file("./path/to/this/file)
+
+# Test that the function labels entrances and exits as expected when data from 2 sensor types is used as input (RFID and 2 pairs of beam breakers here)
+test_that("The function labels entrances and exits as expected using data from RFID and 2 beam breaker pairs", {
   
   # Avoid library calls and other changes to the virtual environment
   # See https://r-pkgs.org/testing-design.html
@@ -48,159 +55,112 @@ test_that("The output is the same regardless of which sensor triggered first", {
   library(testthat)
   
   # Create a temporary directory for testing. Files will be written and read here
-  path <- "/home/gsvidaurre/Desktop/tmp_tests"
+  path <- "/home/gsvidaurre/Desktop"
+  data_dir <- "tmp_tests"
+  tmp_path <- file.path(path, data_dir)
   
-  if(!dir.exists(path)){ 
-    dir.create(path)
+  if(!dir.exists(tmp_path)){ 
+    dir.create(tmp_path)
   }
   
-  # Generate a file with pre-processed timestamps for the RFID antenna, and the outer pair of beam breakers
+  # Generate a file with pre-processed timestamps for two sensor types
   
-  # Create 2 entrance (RFID triggered second) and 2 exit (RFID triggered first) events
-  ent_starts <- as.POSIXct(c(
-    "2023-01-01 01:00:00 EST",
-    "2023-01-01 02:00:00 EST"
-  ))
+  # Create 3 entrance and 3 exit events using different combinations of the RFID and 2 pairs of beam breakers. Each event is a detection cluster of a given duration (see ends below)
+  starts <- as.POSIXct(c(
+    "2023-01-01 01:00:00 EST"
+  )) + seq(1, 300, 50)
   
-  ent_ends <- as.POSIXct(c(
-    "2023-01-01 01:00:01 EST",
-    "2023-01-01 02:00:01 EST"
-  ))
+  ends <- starts + rep(c(5, 10, 15), 2)
   
-  exi_starts <- as.POSIXct(c(
-    "2023-01-01 01:05:00 EST",
-    "2023-01-01 02:05:00 EST"
-  ))
+  starts
+  ends
   
-  exi_ends <- as.POSIXct(c(
-    "2023-01-01 01:05:01 EST",
-    "2023-01-01 02:05:01 EST"
-  ))
-  
-  rfid_ts <- c(ent_starts, exi_ends)
-  outer_ts <- c(ent_ends, exi_starts)
-  
-  # I have to create separate spreadsheets for RFID and IRBB
-  
-  # rfid_file_nm, irbb_file_nm,
-  
-  # sensor_id_col, timestamps_col, PIT_tag_col, outer_irbb_col, inner_irbb_col, irbb_event_col, irbb_unique_col,
-  
-  
-  test_rfid <- data.frame(timestamps_ms = rfid_ts) %>% 
+  # Create a spreadsheet of the simulated directional movement events
+  sim_dats <- data.frame(start = starts) %>% 
     dplyr::mutate(
       chamber_id = "Box_01",
-      year = year(rfid_ts),
-      month = month(rfid_ts),
-      day = day(rfid_ts),
-      sensor_id = "RFID",
-      PIT_tag_ID = "test",
-      thin_threshold_s = 1
-      # data_stage = "pre-processing",
-      # date_pre_processed = Sys.Date()
-    )
-  
-  # Adding outer timestamps twice to simulate inner pair timestamps that the function needs (for adding into the final output). These need to be in separate columns though
-  test_irbb <- data.frame(Outer_beam_breaker = outer_ts) %>% 
-    dplyr::mutate(
-      chamber_id = "Box_01",
-      year = year(outer_ts),
-      month = month(outer_ts),
-      day = day(outer_ts),
-      Inner_beam_breaker = outer_ts,
-      irbb_direction_inferred = rep(c("Entrance", "Exit"), 2),
-      unique_entranceExit = paste(rep(c("Entrance", "Exit"), 2), rep(seq(1, 2, 1), each = 2), sep = "-"),
-      thin_threshold_s = 1
-    )
-  
-  glimpse(test_rfid)
-  glimpse(test_irbb)
-  
-  write.csv(test_rfid, file.path(path, "test_rfid.csv"), row.names = FALSE)
-  write.csv(test_irbb, file.path(path, "test_irbb.csv"), row.names = FALSE)
-  
-  # Test the function
-  source("/home/gsvidaurre/Desktop/GitHub_repos/Abissmal/R/integrate_rfid_breamBreakers.R")
-  
-  # Test the inner code of the function with View(lags_grpd$lags[[1]])
-  # There should be 4 different matches but the function isn't catching a single one. Check the lead and lag RFID to outer IRBB timestamp comparisons carefully
-  # Why aren't there NAs here as in the real datasets?
-  integrate_rfid_beamBreakers(rfid_file_nm = "test_rfid.csv", irbb_file_nm = "test_irbb.csv", l_th = 0, u_th = 2, sensor_id_col = "sensor_id", PIT_tag_col = "PIT_tag_ID", timestamps_col = "timestamps_ms", outer_irbb_col = "Outer_beam_breaker", inner_irbb_col = "Inner_beam_breaker", irbb_event_col = "irbb_direction_inferred", irbb_unique_col = "unique_entranceExit", preproc_metadata_cols = c("thin_threshold_s"), general_metadata_cols = c("chamber_id", "year", "month", "day"), path = "/home/gsvidaurre/Desktop", integrate_perching = FALSE, data_dir = "tmp_tests", out_dir = "tmp_tests", out_file_nm = "integ_rfid_beamBreaker_testData.csv", tz = "America/New York", POSIXct_format = "%Y-%m-%d %H:%M:%OS")
-  
-  # Read in the output, check the output, then delete all files
-  test_res <- read.csv(file.path(path, "integ_rfid_beamBreaker_testData.csv"))
-  glimpse(test_res)
-  
-  # The results should be two repetitions of an entrance followed by an exit
-  expect_equal(test_res$irbb_direction_inferred, rep(c("entrance", "exit"), 2))
-  
-  # Then another test: switching the order of the timestamps between beam breaker pairs should yield 2 repetitions of an exit followed by an entrance
-  inner_ts <- c(ent_starts, exi_ends)
-  outer_ts <- c(ent_ends, exi_starts)
-  
-  test_df <- data.frame(timestamps_ms = c(outer_ts, inner_ts)) %>% 
-    dplyr::mutate(
-      chamber_id = "Box_01",
-      year = c(year(outer_ts), year(inner_ts)),
-      month = c(month(outer_ts), month(inner_ts)),
-      day = c(day(outer_ts), day(inner_ts)),
-      sensor_id = c(rep("Outer beam breaker", length(outer_ts)),
-                    rep("Inner beam breaker", length(inner_ts))),
-      data_type = "IRBB",
-      thin_threshold_s = 1,
+      year = year(start),
+      month = month(start),
+      day = day(start),
+      end = ends,
+      event_seq = c(
+        paste(c(rep("Outer Beam Breaker", 2), rep("Inner Beam Breaker", 2)), collapse = "; "), # entrance
+        paste(c(rep("RFID", 2), rep("Inner Beam Breaker", 1)), collapse = "; "), # entrance
+        paste(c(rep("Outer Beam Breaker", 3), rep("RFID", 2)), collapse = "; "), # entrance
+        paste(c(rep("Inner Beam Breaker", 2), rep("Outer Beam Breaker", 2)), collapse = "; "), # exit
+        paste(c(rep("Inner Beam Breaker", 5), rep("RFID", 2)), collapse = "; "), # exit
+        paste(c(rep("RFID", 2), rep("Outer Beam Breaker", 4)), collapse = "; ") # exit
+      ),
+      indiv1_id = "test",
+      indiv2_id = NA,
+      # Matches the number of RFID detections if present above
+      total_indiv1_detections = c(NA, 2, 3, NA, 2, 4), 
+      total_indiv2_detections = 0,
+      individual_initiated = "test",
+      individual_ended = "test",
+      threshold_seconds = 1,
+      run_length = 1,
       data_stage = "pre-processing",
       date_pre_processed = Sys.Date()
     )
   
-
-  # Read in the output, check the output, then delete all files
-  test_res <- read.csv(file.path(path, "labeled_beamBreaker_testData.csv"))
-  # glimpse(test_res)
+  # glimpse(sim_dats)
+  # View(sim_dats)
   
-  # The results should be two repetitions of an entrance followed by an exit
-  expect_equal(test_res$irbb_direction_inferred, rep(c("exit", "entrance"), 2))
+  write.csv(sim_dats, file.path(tmp_path, "simulated_detectionClusters.csv"), row.names = FALSE)
+
+  
+  # Right now the NULL in camera_label is causing problems in the labeling conditional statements
+  source("/home/gsvidaurre/Desktop/GitHub_repos/Abissmal/R/score_detectionClusters.R")
+  
+  score_detectionClusters(file_nm = "simulated_detectionClusters.csv", sensor_id_col = NULL, PIT_tag_col = NULL, rfid_label = "RFID", camera_label = NULL, outer_irbb_label = "Outer Beam Breaker", inner_irbb_label = "Inner Beam Breaker", general_metadata_cols = c("chamber_id", "year", "month", "day"), video_metadata_cols = NULL, integrate_perching = FALSE, perching_dataset = NULL, perching_prefix = NULL, path = path, data_dir = data_dir, out_dir = data_dir, out_file_nm = "scored_detectionClusters.csv", tz = "America/New York", POSIXct_format = "%Y-%m-%d %H:%M:%OS")
+  
+  # Read in the output, check the output, then delete all files
+  test_res <- read.csv(file.path(tmp_path, "scored_detectionClusters.csv"))
+  
+  View(test_res)
+  
+  # Test that the results are 3 entrance events and 3 exit events, in that order
+  event_labels <- sapply(1:nrow(test_res), function(i){
+    
+    wh <- which(!is.na(test_res[i, grep("direction", names(test_res))]))
+    test_res[i, grep("direction", names(test_res))][[wh]]
+                
+    
+  })
+  
+  expect_equal(event_labels, rep(c("entrance", "exit"), each = 3))
+  
+  # Test that the number of detection clusters in the input and output data are the same
+  expect_equal(nrow(sim_dats), nrow(test_res))
+  
+  # Test that order of sensor labels in the first edge is correct
+  sensor_seq <- sapply(1:nrow(test_res), function(i){
+    
+    paste(unique(strsplit(sim_dats$event_seq[i], split = "; ")[[1]]), collapse = " - ")
+    
+  })
+    
+  invisible(lapply(1:nrow(test_res), function(i){
+    expect_equal(test_res$Edge_1[i], sensor_seq[i])
+  }))
+  
+  # TKTK CONTINUE: the RFID indiv ID columns in the input are not carried through to the output 
+  
+  # Test that the results have the correct number of detections per individual (since RFID data was used as input)
+  test_detectns <- length(c(starts_rfid[1], ends_rfid[1]))
+  
+  invisible(lapply(1:nrow(test_res), function(i){
+    expect_equal(test_res$total_indiv1_detections[i], test_detectns)
+  }))
   
   # Remove the temporary directory and all files within it
-  unlink(path, recursive = TRUE)
+  if(tmp_path == file.path(path, data_dir)){
+    unlink(tmp_path, recursive = TRUE)
+  }
   
 })
 
 
+########## Testing error messages ########## 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-library(tidyverse)
-
-l_th <- 0
-u_th <- 5
-rfid_file_nm <- "pre_processed_data_RFID.csv"
-irbb_file_nm <- "labeled_beamBreaker_data.csv"
-sensor_id_col <- "sensor_id"
-timestamps_col <- "timestamp_ms"
-PIT_tag_col <- "PIT_tag_ID"
-outer_irbb_col <- "Outer_beam_breaker"
-inner_irbb_col <- "Inner_beam_breaker"
-irbb_event_col <- "irbb_direction_inferred"
-irbb_unique_col <- "unique_entranceExit"
-preproc_metadata_cols <- c("thin_threshold_s", "data_stage", "date_pre_processed", "lower_threshold_s", "upper_threshold_s", "date_labeled")
-method <- "temporal"
-path <- "/media/gsvidaurre/Anodorhynchus/Data_Testing/Box_02_31Dec2022/Data"
-data_dir <- "pre_processed"
-out_dir <- "integrated"
-tz <- "America/New York"
-POSIXct_format <- "%Y-%m-%d %H:%M:%OS"
-integrate_perching <- TRUE
-
-general_metadata_cols
