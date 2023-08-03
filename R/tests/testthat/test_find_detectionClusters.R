3f69bfa45bc36fd7fdeb15cb5950cc8d6a61b8f4
Grace Smith Vidaurre <gsmithvi@gmail.com>
2023-08-02 21:05:37 -0400

    Started unit testing for another function; continue with fixing a bug in individual ID columns failing to appear in the output

--- R/tests/testthat/test_find_detectionClusters.R
+++ R/tests/testthat/test_find_detectionClusters.R
@@ -1,9 +1,4 @@
 
-# See examples on: 
-# https://www.r-bloggers.com/2019/11/automated-testing-with-testthat-in-practice/
-
-rm(list = ls())
-
 if (!require(testthat)) install.packages('testthat')
 library(testthat)
 
@@ -761,39 +756,6 @@ test_that("The function detects the expected number of clusters using data from 
 })
 
 
-
-
-
 ########## Testing error messages ########## 
 
 
-
-
-
-
-
-
-
-library(tidyverse)
-
-l_th <- 0
-u_th <- 5
-rfid_file_nm <- "pre_processed_data_RFID.csv"
-irbb_file_nm <- "labeled_beamBreaker_data.csv"
-sensor_id_col <- "sensor_id"
-timestamps_col <- "timestamp_ms"
-PIT_tag_col <- "PIT_tag_ID"
-outer_irbb_col <- "Outer_beam_breaker"
-inner_irbb_col <- "Inner_beam_breaker"
-irbb_event_col <- "irbb_direction_inferred"
-irbb_unique_col <- "unique_entranceExit"
-preproc_metadata_cols <- c("thin_threshold_s", "data_stage", "date_pre_processed", "lower_threshold_s", "upper_threshold_s", "date_labeled")
-method <- "temporal"
-path <- "/media/gsvidaurre/Anodorhynchus/Data_Testing/Box_02_31Dec2022/Data"
-data_dir <- "pre_processed"
-out_dir <- "integrated"
-tz <- "America/New York"
-POSIXct_format <- "%Y-%m-%d %H:%M:%OS"
-integrate_perching <- TRUE
-
-general_metadata_cols
