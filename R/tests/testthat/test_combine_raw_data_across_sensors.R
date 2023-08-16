507016c32d46f0e0f10f0a9de77e66ed3bbebfb2
Grace Smith Vidaurre <gsmithvi@gmail.com>
2023-08-16 13:22:22 -0400

    Finished building the current round of unit tests for this function

--- R/tests/testthat/test_combine_raw_data_across_sensors.R
+++ R/tests/testthat/test_combine_raw_data_across_sensors.R
@@ -42,7 +42,6 @@ test_that("The function combines RFID data collected across dates", {
   
   # Generate files of raw RFID data per unique date
   dates <- c("2023-8-1", "2023-8-2", "2023-8-3")
-  dates
   
   # Create a vector of RFID timestamps. Each file will have 4 timestamps
   event_ts <- c(
@@ -88,9 +87,434 @@ test_that("The function combines RFID data collected across dates", {
 })
 
 # Test that IRBB data from 2 beam breaker pairs collected across dates is combined into a single file
+test_that("The function combines IRBB data collected across dates", {
+  
+  # Avoid library calls and other changes to the virtual environment
+  # See https://r-pkgs.org/testing-design.html
+  withr::local_package("tidyverse")
+  withr::local_package("plyr")
+  withr::local_package("dplyr")
+  withr::local_package("lubridate")
+  withr::local_package("pbapply")
+  
+  # Just for code development
+  # library(tidyverse)
+  # library(lubridate)
+  # library(pbapply)
+  # library(testthat)
+  # library(data.table)
+  
+  # Create a temporary directory for testing. Files will be written and read here
+  path <- "/home/gsvidaurre/Desktop"
+  data_dir <- "tmp_tests"
+  tmp_path <- file.path(path, data_dir)
+  
+  if(!dir.exists(tmp_path)){ 
+    dir.create(tmp_path)
+  }
+  
+  # Then create an IRBB folder inside this path
+  dir.create(file.path(tmp_path, "IRBB"))
+  
+  # Generate files of raw IRBB data per unique date
+  dates <- c("2023-8-1", "2023-8-2", "2023-8-3")
+  
+  # Create a vector of inner and outer beam breaker timestamps. Each file will have 8 timestamps (4 per beam breaker pair)
+  event_ts_o <- c(
+    "01:00:00",
+    "02:00:00",
+    "01:05:00",
+    "02:05:00"
+  )
+  
+  event_ts_i <- c(
+    "01:00:02",
+    "02:00:02",
+    "01:05:02",
+    "02:05:02"
+  )
+  
+  # Write out a spreadsheet per date with these timestamps that will be used as input data for the function
+  invisible(lapply(1:length(dates), function(i){
+    
+    sim_ts <- data.frame(timestamp_ms = c(event_ts_o, event_ts_i)) %>% 
+      dplyr::mutate(
+        chamber_id = "Box_01",
+        year = year(dates[i]),
+        month = month(dates[i]),
+        day = day(dates[i]),
+        sensor_id = c(rep("Outer Beam Breaker", length(event_ts_o)), rep("Inner Beam Breaker", length(event_ts_i)))
+      )
+    
+    write.csv(sim_ts, file.path(tmp_path, "IRBB", paste("IRBB_Box_01_", gsub("-", "_", dates[i]), ".csv", sep = "")), row.names = FALSE)
+    
+  }))
+  
+  combine_raw_data_per_sensor(sensors = "IRBB", path = path, data_dir = data_dir, out_dir = file.path(data_dir, "raw_combined"), tz = "America/New York", POSIXct_format = "%Y-%m-%d %H:%M:%OS")
+  
+  # Read in the output, check the output, then delete all files
+  test_res <- read.csv(file.path(tmp_path, "raw_combined", "combined_raw_data_IRBB.csv"))
+  
+  # Test that the results are the expected number of timestamps per date
+  expect_equal(nrow(test_res), (length(event_ts_o) + length(event_ts_i))* length(dates))
+  
+  # Test that all expected dates are in the combined file
+  expect_equal(unique(paste(test_res$year, test_res$month, test_res$day, sep = "_")), gsub("-", "_", dates))
+  
+  # Remove the temporary directory and all files within it
+  if(tmp_path == file.path(path, data_dir)){
+    unlink(tmp_path, recursive = TRUE)
+  }
+  
+})
 
 # Test that video recording data collected across dates is combined into a single file
+test_that("The function combines Video data collected across dates", {
+  
+  # Avoid library calls and other changes to the virtual environment
+  # See https://r-pkgs.org/testing-design.html
+  withr::local_package("tidyverse")
+  withr::local_package("plyr")
+  withr::local_package("dplyr")
+  withr::local_package("lubridate")
+  withr::local_package("pbapply")
+  
+  # Just for code development
+  # library(tidyverse)
+  # library(lubridate)
+  # library(pbapply)
+  # library(testthat)
+  # library(data.table)
+  
+  # Create a temporary directory for testing. Files will be written and read here
+  path <- "/home/gsvidaurre/Desktop"
+  data_dir <- "tmp_tests"
+  tmp_path <- file.path(path, data_dir)
+  
+  if(!dir.exists(tmp_path)){ 
+    dir.create(tmp_path)
+  }
+  
+  # Then create a Video folder inside this path
+  dir.create(file.path(tmp_path, "Video"))
+  
+  # Generate files of raw Video recording events data per unique date
+  dates <- c("2023-8-1", "2023-8-2", "2023-8-3")
+  
+  # Create a vector of timestamps of video recording events. Each file will have 4 timestamps
+  event_ts <- c(
+    "01:00:00",
+    "02:00:00",
+    "01:05:00",
+    "02:05:00"
+  )
+  
+  # Write out a spreadsheet per date with these timestamps that will be used as input data for the function. Each timestamp is repeated twice to account for a pre- and post-motion trigger video recording per timestamp
+  invisible(lapply(1:length(dates), function(i){
+    
+    file_nms <- paste(paste(rep(paste(paste("Box_01", gsub("-", "_", dates[i]), sep = "_"), gsub(":", "_", event_ts), sep = "_"), each = 2), c("pre_trigger", "post_trigger"), sep = "_"), ".mp4", sep = "")
+    
+    sim_ts <- data.frame(time_video_started = rep(event_ts, each = 2)) %>% 
+      dplyr::mutate(
+        chamber_id = "Box_01",
+        year = year(dates[i]),
+        month = month(dates[i]),
+        day = day(dates[i]),
+        sensor_id = "Camera",
+        video_file_name = file_nms,
+        total_pixels_motionTrigger = 100
+      )
+    
+    write.csv(sim_ts, file.path(tmp_path, "Video", paste("Video_Box_01_", gsub("-", "_", dates[i]), ".csv", sep = "")), row.names = FALSE)
+    
+  }))
+  
+  combine_raw_data_per_sensor(sensors = "Video", path = path, data_dir = data_dir, out_dir = file.path(data_dir, "raw_combined"), tz = "America/New York", POSIXct_format = "%Y-%m-%d %H:%M:%OS")
+  
+  # Read in the output, check the output, then delete all files
+  test_res <- read.csv(file.path(tmp_path, "raw_combined", "combined_raw_data_Video.csv"))
+  
+  # Test that the results are the expected number of timestamps per date
+  expect_equal(nrow(test_res), (length(event_ts) * 2) * length(dates))
+  
+  # Test that all expected dates are in the combined file
+  expect_equal(unique(paste(test_res$year, test_res$month, test_res$day, sep = "_")), gsub("-", "_", dates))
+  
+  # Remove the temporary directory and all files within it
+  if(tmp_path == file.path(path, data_dir)){
+    unlink(tmp_path, recursive = TRUE)
+  }
+  
+})
 
 # Test that temperature data collected across dates is combined into a single file
+test_that("The function combines temperature data collected across dates", {
+  
+  # Avoid library calls and other changes to the virtual environment
+  # See https://r-pkgs.org/testing-design.html
+  withr::local_package("tidyverse")
+  withr::local_package("plyr")
+  withr::local_package("dplyr")
+  withr::local_package("lubridate")
+  withr::local_package("pbapply")
+  
+  # Just for code development
+  # library(tidyverse)
+  # library(lubridate)
+  # library(pbapply)
+  # library(testthat)
+  # library(data.table)
+  
+  # Create a temporary directory for testing. Files will be written and read here
+  path <- "/home/gsvidaurre/Desktop"
+  data_dir <- "tmp_tests"
+  tmp_path <- file.path(path, data_dir)
+  
+  if(!dir.exists(tmp_path)){ 
+    dir.create(tmp_path)
+  }
+  
+  # Then create a Temperature folder inside this path
+  dir.create(file.path(tmp_path, "Temp"))
+  
+  # Generate files of raw temperature data per unique date
+  dates <- c("2023-8-1", "2023-8-2", "2023-8-3")
 
-# Test that data collected across all sensor types and dates is combined into a single file
+  # Create a vector of timestamps of temperature measurements taken each minute. Each file will have 4 timestamps
+  event_ts <- c(
+    "01:00:00",
+    "01:01:00",
+    "01:02:00",
+    "01:03:00"
+  )
+  
+  # Write out a spreadsheet per date with these timestamps that will be used as input data for the function
+  invisible(lapply(1:length(dates), function(i){
+
+    sim_ts <- data.frame(time = event_ts) %>% 
+      dplyr::mutate(
+        chamber_id = "Box_01",
+        year = year(dates[i]),
+        month = month(dates[i]),
+        day = day(dates[i]),
+        sensor_id = "Temp",
+        degrees_Celsius = 23.9,
+        degrees_Farenheit = 75
+      )
+    
+    write.csv(sim_ts, file.path(tmp_path, "Temp", paste("Temp_Box_01_", gsub("-", "_", dates[i]), ".csv", sep = "")), row.names = FALSE)
+    
+  }))
+  
+  combine_raw_data_per_sensor(sensors = "Temp", path = path, data_dir = data_dir, out_dir = file.path(data_dir, "raw_combined"), tz = "America/New York", POSIXct_format = "%Y-%m-%d %H:%M:%OS")
+  
+  # Read in the output, check the output, then delete all files
+  test_res <- read.csv(file.path(tmp_path, "raw_combined", "combined_raw_data_Temp.csv"))
+  
+  # Test that the results are the expected number of timestamps per date
+  expect_equal(nrow(test_res), length(event_ts) * length(dates))
+  
+  # Test that all expected dates are in the combined file
+  expect_equal(unique(paste(test_res$year, test_res$month, test_res$day, sep = "_")), gsub("-", "_", dates))
+  
+  # Remove the temporary directory and all files within it
+  if(tmp_path == file.path(path, data_dir)){
+    unlink(tmp_path, recursive = TRUE)
+  }
+  
+})
+
+# Test that data collected across all 4 sensor types and dates is combined into a single file
+test_that("The function combines data from all 4 sensor types collected across dates", {
+  
+  # Avoid library calls and other changes to the virtual environment
+  # See https://r-pkgs.org/testing-design.html
+  withr::local_package("tidyverse")
+  withr::local_package("plyr")
+  withr::local_package("dplyr")
+  withr::local_package("lubridate")
+  withr::local_package("pbapply")
+  
+  # Just for code development
+  # library(tidyverse)
+  # library(lubridate)
+  # library(pbapply)
+  # library(testthat)
+  # library(data.table)
+  
+  # Create a temporary directory for testing. Files will be written and read here
+  path <- "/home/gsvidaurre/Desktop"
+  data_dir <- "tmp_tests"
+  tmp_path <- file.path(path, data_dir)
+  
+  if(!dir.exists(tmp_path)){ 
+    dir.create(tmp_path)
+  }
+  
+  # Then create a folders for each sensor type inside this path
+  sensrs <- c("RFID", "IRBB", "Video", "Temp")
+  
+  invisible(lapply(1:length(sensrs), function(i){
+    
+    dir.create(file.path(tmp_path, sensrs[i]))
+    
+  }))
+  
+  # Generate files of raw temperature data per unique date
+  dates <- c("2023-8-1", "2023-8-2", "2023-8-3")
+  
+  ###### RFID ######
+  
+  # Create a vector of RFID timestamps. Each file will have 4 timestamps
+  rfid_event_ts <- c(
+    "01:00:00",
+    "02:00:00",
+    "01:05:00",
+    "02:05:00"
+  )
+  
+  # Write out a spreadsheet per date with these timestamps that will be used as input data for the function
+  invisible(lapply(1:length(dates), function(i){
+    
+    sim_ts <- data.frame(timestamp_ms = rfid_event_ts) %>% 
+      dplyr::mutate(
+        chamber_id = "Box_01",
+        year = year(dates[i]),
+        month = month(dates[i]),
+        day = day(dates[i]),
+        sensor_id = "RFID",
+        PIT_tag_ID = "test"
+      )
+    
+    write.csv(sim_ts, file.path(tmp_path, "RFID", paste("RFID_Box_01_", gsub("-", "_", dates[i]), ".csv", sep = "")), row.names = FALSE)
+    
+  }))
+
+  ###### IRBB ######
+  
+  # Create a vector of inner and outer beam breaker timestamps. Each file will have 8 timestamps (4 per beam breaker pair)
+  event_ts_o <- c(
+    "01:00:00",
+    "02:00:00",
+    "01:05:00",
+    "02:05:00"
+  )
+  
+  event_ts_i <- c(
+    "01:00:02",
+    "02:00:02",
+    "01:05:02",
+    "02:05:02"
+  )
+  
+  # Write out a spreadsheet per date with these timestamps that will be used as input data for the function
+  invisible(lapply(1:length(dates), function(i){
+    
+    sim_ts <- data.frame(timestamp_ms = c(event_ts_o, event_ts_i)) %>% 
+      dplyr::mutate(
+        chamber_id = "Box_01",
+        year = year(dates[i]),
+        month = month(dates[i]),
+        day = day(dates[i]),
+        sensor_id = c(rep("Outer Beam Breaker", length(event_ts_o)), rep("Inner Beam Breaker", length(event_ts_i)))
+      )
+    
+    write.csv(sim_ts, file.path(tmp_path, "IRBB", paste("IRBB_Box_01_", gsub("-", "_", dates[i]), ".csv", sep = "")), row.names = FALSE)
+    
+  }))
+  
+  ###### Video ######
+  
+  # Create a vector of timestamps of video recording events. Each file will have 4 timestamps
+  video_event_ts <- c(
+    "01:00:00",
+    "02:00:00",
+    "01:05:00",
+    "02:05:00"
+  )
+  
+  # Write out a spreadsheet per date with these timestamps that will be used as input data for the function. Each timestamp is repeated twice to account for a pre- and post-motion trigger video recording per timestamp
+  invisible(lapply(1:length(dates), function(i){
+    
+    file_nms <- paste(paste(rep(paste(paste("Box_01", gsub("-", "_", dates[i]), sep = "_"), gsub(":", "_", video_event_ts), sep = "_"), each = 2), c("pre_trigger", "post_trigger"), sep = "_"), ".mp4", sep = "")
+    
+    sim_ts <- data.frame(time_video_started = rep(video_event_ts, each = 2)) %>% 
+      dplyr::mutate(
+        chamber_id = "Box_01",
+        year = year(dates[i]),
+        month = month(dates[i]),
+        day = day(dates[i]),
+        sensor_id = "Camera",
+        video_file_name = file_nms,
+        total_pixels_motionTrigger = 100
+      )
+    
+    write.csv(sim_ts, file.path(tmp_path, "Video", paste("Video_Box_01_", gsub("-", "_", dates[i]), ".csv", sep = "")), row.names = FALSE)
+    
+  }))
+  
+  ###### Temperature ######
+  
+  # Create a vector of timestamps of temperature measurements taken each minute. Each file will have 4 timestamps
+  temp_event_ts <- c(
+    "01:00:00",
+    "01:01:00",
+    "01:02:00",
+    "01:03:00"
+  )
+  
+  # Write out a spreadsheet per date with these timestamps that will be used as input data for the function
+  invisible(lapply(1:length(dates), function(i){
+    
+    sim_ts <- data.frame(time = temp_event_ts) %>% 
+      dplyr::mutate(
+        chamber_id = "Box_01",
+        year = year(dates[i]),
+        month = month(dates[i]),
+        day = day(dates[i]),
+        sensor_id = "Temp",
+        degrees_Celsius = 23.9,
+        degrees_Farenheit = 75
+      )
+    
+    write.csv(sim_ts, file.path(tmp_path, "Temp", paste("Temp_Box_01_", gsub("-", "_", dates[i]), ".csv", sep = "")), row.names = FALSE)
+    
+  }))
+  
+  combine_raw_data_per_sensor(sensors = sensrs, path = path, data_dir = data_dir, out_dir = file.path(data_dir, "raw_combined"), tz = "America/New York", POSIXct_format = "%Y-%m-%d %H:%M:%OS")
+  
+  # For each sensor type, read in and check the output before deleting temporary files
+  invisible(lapply(1:length(sensrs), function(i){
+    
+    test_res <- read.csv(file.path(tmp_path, "raw_combined", paste("combined_raw_data_", sensrs[i], ".csv", sep = "")))
+    
+    # Test that the results are the expected number of timestamps per date
+    if(sensrs[i] == "RFID"){
+      
+      expect_equal(nrow(test_res), length(rfid_event_ts) * length(dates))
+      
+    }  else if(sensrs[i] == "IRBB"){
+      
+      expect_equal(nrow(test_res), (length(event_ts_o) + length(event_ts_i)) * length(dates))
+      
+    } else if(sensrs[i] == "Video"){
+      
+      expect_equal(nrow(test_res), (length(video_event_ts) * 2) * length(dates))
+      
+    } else if(sensrs[i] == "Temp"){
+      
+      expect_equal(nrow(test_res), length(temp_event_ts) * length(dates))
+      
+    }
+    
+    # Test that all expected dates are in the combined file
+    expect_equal(unique(paste(test_res$year, test_res$month, test_res$day, sep = "_")), gsub("-", "_", dates))
+    
+  }))
+  
+  # Remove the temporary directory and all files within it
+  if(tmp_path == file.path(path, data_dir)){
+    unlink(tmp_path, recursive = TRUE)
+  }
+  
+})
